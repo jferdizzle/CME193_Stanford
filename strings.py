0f96391ea8a3fb4cf7f38364b0085439cbc9aeea
Julian Ferdman <julianferdman@gmail.com>
2015-06-19 14:15:33 -0400

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,17 @@
+def str1(a):
+    return a * 3
+
+def str2(a, b):
+    return a + b * 3 + a
+
+def str3(a, b, c):
+    return (a + b + c) * 2
+
+
+def str4(a, b, c, d):
+    if len(c) == len(d):
+        return a + b
+    else:
+        return b*2 + c*2
+
+
